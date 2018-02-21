9e210e0513e3347b20444c06778035bef3b30028
filipolszewski <filipolszew@o2.pl>
2018-02-21 14:09:31 +0100

    nn with backprop on naive loops

--- playground/simple-NN-pure-python/Network.py
+++ playground/simple-NN-pure-python/Network.py
@@ -23,33 +23,98 @@ class Network:
         self.biases = [[random.random() - 0.5 for b in range(self.l_sizes[l])] for l in range(1, self.num_layers)]
 
     @staticmethod
-    def calc_loss(self, y_predicted, y_true):
-        """tu bedzie docstring metody calc_loss"""
+    def calc_loss(y_predicted, y_true):
+        """zwraca listę - każdy element to wynik funkcji loss dla poszczególnego output neurona"""
 
-        loss = []
+        return [1/2 * (y_t - y_p) ** 2 for y_t, y_p in zip(y_true, y_predicted)]
 
-        return (1/2) * (1 - y_predicted[y_true]) ** 2
+    def tanh_derivative(self, x):
+        return 1 - tanh(x) ** 2
 
     def forward(self, x):
         """tu bedzie docstring metody forward"""
 
         activations = [x]
+        zs = []
 
         for layer in range(1, self.num_layers):
             a_layer = []
+            z_layer = []
             for neuron in range(self.l_sizes[layer]):
                 weighted_sum = 0
                 for i, weight in enumerate(self.weights[layer-1][neuron]):
                     weighted_sum += weight * activations[layer-1][i]
+                z_layer.append(weighted_sum)
                 a_layer.append(tanh(weighted_sum + self.biases[layer-1][neuron]))
+            zs.append(z_layer)
             activations.append(a_layer)
 
-        return activations
+        return zs, activations
 
-    def backprop(self, loss, activations):
+    def backprop(self, zs, activations, y_true, activ_fn_derivative, l_rate):
 
         """TODO: backprop for any num of layers. now for 3 layers"""
 
+        # output layer gradients
+        # NOTE: this part works for any number of hidden layers because of 'minus indexing'
+        # TODO: maybe we can do 1 loop for output and hidden layers? or maybe not
+        ob_gradients = []
+        ow_gradients = []
+        # for every neuron in output layer
+        for neuron in range(self.l_sizes[-1]):
+            # calculate delta - equation in the blog post
+            # first term is derivative of our cost funtion
+            # TODO: use external function cost_derivative() or smth...
+            # Note: zs[-1] == zs[1] for 3 layers, because {len(zs) = len(activations)-1}, because we don't have zs for input layer
+            delta = (activations[-1][neuron] - y_true[neuron]) * activ_fn_derivative(zs[-1][neuron])
+            # grad for output layer biases is just the delta
+            ob_gradients.append(delta)
 
+            # grad for a certain weight is the delta times activation that 'this weight weights'
+            # Note: this fragment works for any num of hidden layers because of '-2' indexing
+            neuron_w_gradients = []
+            for before_node in range(self.l_sizes[-2]):
+                neuron_w_gradients.append(delta * activations[-2][before_node])
+            ow_gradients.append(neuron_w_gradients)
 
-        pass
+        # hidden layer
+        # TODO: try to make a loop to make it work for any number of hidden layers
+
+        hb_gradients = []
+        hw_gradients = []
+
+        # TODO: self.l_sizes[1] works only if we have 3 layers
+        layer = 1
+        for neuron_k in range(self.l_sizes[layer]):
+            # calculate delta. It is more complicated this time (see blog post). its:
+            # delta = tanh_derivative * sum[for each neuron in layer+1](delta_j * weight_jk)
+            # where weight_jk is the one between current neuron k and neuron j from layer+1
+            # delta_j is delta for the neuron j from layer+1
+            sum_of_weighted_output_error = 0
+            for neuron_j in range(self.l_sizes[layer+1]):
+                # ob_gradients[0] is the same as delta_j so we use it instead of remembering deltas
+                # NOTE: weights[layer] are weights of next layer, not current! coz we don't have weights for input layer
+                sum_of_weighted_output_error += ob_gradients[0] * self.weights[layer][neuron_j][neuron_k]
+            delta = sum_of_weighted_output_error * activ_fn_derivative(zs[0][neuron_k])
+            hb_gradients.append(delta)
+
+            # gradients of hidden weights - see blog post. We use the current delta and activations from last layer
+            neuron_hw_gradients = []
+            for before_node in range(self.l_sizes[layer-1]):
+                neuron_hw_gradients.append(delta * activations[layer-1][before_node])
+            hw_gradients.append(neuron_hw_gradients)
+
+        # update parameters
+        # TODO: make it work for any number of layers, and try to merge loops into one.
+        # output layer
+        layer = 2
+        for neuron in range(self.l_sizes[layer]):
+            self.biases[layer-1][neuron] -= l_rate * ob_gradients[neuron]
+            for w in range(self.l_sizes[layer - 1]):
+                self.weights[layer-1][neuron][w] -= l_rate * ow_gradients[neuron][w]
+        # hidden layer
+        layer = 1
+        for neuron in range(self.l_sizes[layer]):
+            self.biases[layer-1][neuron] -= l_rate * hb_gradients[neuron]
+            for w in range(self.l_sizes[layer - 1]):
+                self.weights[layer-1][neuron][w] -= l_rate * hw_gradients[neuron][w]
--- playground/simple-NN-pure-python/main.py
+++ playground/simple-NN-pure-python/main.py
@@ -7,28 +7,37 @@ random.seed(1)
 
 
 # inicjalizacja wszystkiego
-set_size = 100
-X, Y = datasets.make_moons(set_size, noise=0.1)
-trainX, trainY = X[:int(0.8 * set_size)], Y[:int(0.8 * set_size)]
-testX, testY = X[int(0.8 * set_size):], Y[int(0.8 * set_size):]
+l_rate = 0.01
+set_size = 10000
+X, Y = datasets.make_moons(set_size, noise=0)
+split_ratio = 0.90
+train_set_size = int(split_ratio * set_size)
+trainX, trainY = X[:train_set_size], Y[:train_set_size]
+testX, testY = X[train_set_size:], Y[train_set_size:]
 
-network = Network(l_sizes=(2, 4, 2))
-
-print(network.forward(trainX[0]))
-
-exit(1)
+l_sizes = (2, 4, 2)
+network = Network(l_sizes)
 
 # uczenie sieci
+print("Learning the net...")
 for index, x in enumerate(trainX):
-    activations = network.forward(x)
-    loss = network.calc_loss(activations[-1], trainY[index])
-    network.backprop(loss, activations)
+    if index % 1000 == 0:
+        print("{0:.2f} %".format(round(index/train_set_size * 100, 2)))
+    zs, activations = network.forward(x)
+    y_true = [0] * l_sizes[-1]
+    # zamiast y które wskazuje klasę, tworzę wektor mówiący o oczekiwanej wartości dla danego output neorun
+    y_true[trainY[index]] = 1
+    network.backprop(zs, activations, y_true, network.tanh_derivative, l_rate)
 
+print("Learning done.")
 # ewaluacja na zbiorze testowym
+
+print("Testing the net...")
 accuracy_sum = 0
 for index, x in enumerate(testX):
-    y_predicted = network.forward(x)
-    predicted_class = y_predicted.index(max(y_predicted))
+    zs, activations = network.forward(x)
+    predicted_class = activations[-1].index(max(activations[-1]))
     accuracy_sum += not (predicted_class ^ testY[index]) # ^ oznacza XOR
 
+print("Testing done. Score:")
 print(accuracy_sum / len(testX))
