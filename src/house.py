86d268f1049ad20bc62d7ab27e78a9479133f912
filipolszewski <filipolszew@o2.pl>
2018-03-14 20:27:50 +0100

    reward function impl

--- src/house.py
+++ src/house.py
@@ -4,38 +4,43 @@ class House():
 
     def __init__(self):
 
-        self.pv_absorption = 2000 # Watt on max sun intensity. pv->photovoltaic
-        self.grid_cost = 0.5 # PLN for 1kWh
+        self.day_start = None
+        self.day_end = None
+        self.pv_absorption = 2000  # Watt on max sun intensity. pv->photovoltaic
+        self.grid_cost = 0.5  # PLN for 1kWh
         self.battery = {
-            'current' : 0,
-            'max' : 14000 # Watt, thats as good as single Tesla PowerWall unit.
+            'current': 0,
+            'max': 14000  # Watt, thats as good as single Tesla PowerWall unit.
         }
 
         self.user_requests = {
-            'day' : {
-                'temp_min' : 20,
-                'temp_max' : 22,
-                'light_min' : 0.7 
+            'day': {
+                'temp_desired': 21,
+                'temp_epsilon': 0.5,
+                'light_desired': 0.7,
+                'light_epsilon': 0.05
             },
-            'night' : {
-                'temp_min' : 17,
-                'temp_max' : 20,
+            'night': {
+                'temp_desired': 18,
+                'temp_epsilon': 1,
+                'light_desired': 0.0,
+                'light_epsilon': 0.01
             }
         }
 
         self.inside_sensors = {
             'first' : {
-                'temperature' : 0,
-                'light' : 0
+                'temperature': 0,
+                'light': 0
             }
         }
 
         self.current_settings = {
-            'energy_src' : 'grid',
-            'cooling_lvl' : 0,
-            'heating_lvl' : 0,
-            'light_lvl' : 0,
-            'curtains_lvl' : 0 
+            'energy_src': 'grid',
+            'cooling_lvl': 0,
+            'heating_lvl': 0,
+            'light_lvl': 0,
+            'curtains_lvl': 0
         }
 
         self.actions = [
@@ -66,14 +71,63 @@ class House():
 
     def _calculate_energy_cost(self):
         # TODO: implement me!
-        pass
+        return 0
 
     def reward(self):
-        #TODO: Given values from inside sensors, energy costs and user requests,
-        # calculate reward
-        reward = 0
-        return reward
-        
+        """
+        Calculate reward for last timeframe. Note that the reward in the whole simulator is always non-positive,
+        so it is easier to interpret as penalty.
+        Return value depends on:
+        - penalty for temperature level
+        - penalty for light level
+        - penalty for cost of used energy
+
+        Function is parametrized by weights for every penalty and by exponents (for temp and light penalties)
+        Increase weight to make the corresponding penalty more important.
+        Increase exponent to make the corresponding penalty bigger with increasing difference
+            (see _calculate_penalty() method)
+
+        :return: weighted sum of penalties
+        """
+
+        w_temp, w_light, w_cost = 1.0, 1.0, 1.0
+        temp_exponent, light_exponent = 2, 2
+
+        cost = self._calculate_energy_cost()
+        temp, light = self.inside_sensors['first']['temperature'], self.inside_sensors['first']['light']
+        req = self._get_current_user_requests()
+
+        temp_penalty = self._calculate_penalty(temp, req['temp_desired'], req['temp_epsilon'], temp_exponent)
+        light_penalty = self._calculate_penalty(light, req['light_desired'], req['light_epsilon'], light_exponent)
+
+        return -1 * ((cost * w_cost) + (temp_penalty * w_temp) + (light_penalty * w_light))
+
+    def _get_current_user_requests(self):
+        """
+        :return: user requests corresponding to current time (day or night)
+        """
+
+        if self.day_start <= self.daytime < self.day_end:
+            return self.user_requests['day']
+        else:
+            return self.user_requests['night']
+
+    @staticmethod
+    def _calculate_penalty(current, desired, epsilon, power):
+        """
+        Calculates the penalty for given param with small region of acceptance (epsilon)
+        :param current: current param value
+        :param desired: desired param value
+        :param epsilon: acceptable value of difference
+        :param power: exponent of the pow() method
+        :return: penalty for difference between current and desired param
+        """
+
+        difference = current - desired
+        if abs(difference) > epsilon:
+            return pow(difference, power)
+        else:
+            return 0
 
     # from this point, define house actions. add new actions to self.actions too
 
@@ -115,4 +169,6 @@ class House():
     
     def nop(self):
         pass
+
+
                     
---
+++ src/tests/house_api_tests.py
@@ -0,0 +1,122 @@
+import unittest
+from unittest.mock import MagicMock
+import os, sys
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from house import House
+
+
+class BasicHouseTestCase(unittest.TestCase):
+    """Testing house usage and methods"""
+
+    def setUp(self):
+        self.house = House()
+        self.house.day_start = 7 * 60
+        self.house.day_end = 24 * 60 - 5 * 60
+
+    def test_reward(self):
+        """
+        Tests whether the reward function:
+        1. Returns 0 if the parameters are perfect (no difference between expected and desired, no energy cost)
+        2. Returns bigger penalty when the difference increases
+        """
+        # case 1
+        # given
+        self.house._calculate_energy_cost = MagicMock(return_value=0)
+        user_req_mock = {
+                'temp_desired': 21,
+                'temp_epsilon': 0.5,
+                'light_desired': 0.7,
+                'light_epsilon': 0.05
+            }
+        self.house._get_current_user_requests = MagicMock(return_value=user_req_mock)
+
+        self.house.inside_sensors = {
+            'first': {
+                'temperature': 21,
+                'light': 0.7
+            }
+        }
+
+        # when
+        reward = self.house.reward()
+
+        # then
+        self.assertEqual(reward, 0, "Reward should be zero, params are perfect!")
+
+        # case 2
+        # given
+        self.house._calculate_energy_cost = MagicMock(return_value=100)
+        self.house.inside_sensors = {
+            'first': {
+                'temperature': 19,
+                'light': 0.5
+            }
+        }
+
+        # when
+        reward_2 = self.house.reward()
+
+        # then
+        # minus because rewards are negative
+        self.assertGreater(-reward_2, -reward, "Reward should be bigger, parameters are worse.")
+
+
+    def test_get_current_user_requests(self):
+        """
+        Tests if the method returns correct user requests based on the time of the day (night/day requests)
+
+        """
+        # given
+
+        self.house.daytime = 2 * 60  # night
+
+        # when
+        requests = self.house._get_current_user_requests()
+
+        # then
+        self.assertDictEqual(requests, self.house.user_requests['night'], "Method returns user requests for the day, "
+                                                                          "not for the night")
+        # given
+        self.house.daytime = 12 * 60  # day
+
+        # when
+        requests = self.house._get_current_user_requests()
+
+        # then
+        self.assertDictEqual(requests, self.house.user_requests['day'], "Method returns user requests for the night, "
+                                                                        "not for the day")
+
+    def test_calculate_penalty(self):
+        """
+        Tests if the method correctly calculates the penalty, given all the parameters
+
+        """
+
+        # given
+        temp_current = 20.0
+        temp_desired = 21.0
+        temp_epsilon = 1.0
+        power = 2
+
+        # when
+        penalty = House._calculate_penalty(temp_current, temp_desired, temp_epsilon, power)
+
+        # then
+        self.assertEqual(penalty, 0, "Penalty calculated incorrectly, difference is not greater than epsilon!")
+
+        # given
+        temp_current = 100
+        temp_desired = 50
+        temp_epsilon = 1.0
+        power = 2
+
+        # when
+        penalty = House._calculate_penalty(temp_current, temp_desired, temp_epsilon, power)
+
+        # then
+        self.assertEqual(penalty, 2500, "Penalty calculated incorrectly!")
+
+
+if __name__ == '__main__':
+    unittest.main()
