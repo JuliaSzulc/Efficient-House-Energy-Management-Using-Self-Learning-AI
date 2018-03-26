bf924a86bb9d0aa28a60d4e258447613740c47f4
filipolszewski <filipolszew@o2.pl>
2018-03-26 19:59:57 +0200

    learn v1 - not tested

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,7 @@
 import numpy as np
+import random
 import torch
+from torch import autograd, nn, optim
 import torch.nn.functional as F
 
 
@@ -8,19 +10,19 @@ class Net(torch.nn.Module):
         Simple Neural Network.
 
     """
-    def __init__(self, input_neurons, hidden_neurons, output_neurons):
+    def __init__(self, input_size, hidden1_size, hidden2_size, output_size):
         super().__init__()
-        self.fc1 = torch.nn.Linear(input_neurons, hidden_neurons)
-        self.fc2 = torch.nn.Linear(hidden_neurons, hidden_neurons)
-        self.fc3 = torch.nn.Linear(hidden_neurons, output_neurons)
+        self.fc1 = torch.nn.Linear(input_size, hidden1_size)
+        self.fc2 = torch.nn.Linear(hidden1_size, hidden2_size)
+        self.fc3 = torch.nn.Linear(hidden2_size, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = F.tanh(x)   # Hyperbolic tangent activation function
+        x = F.relu(x)
         x = self.fc2(x)
-        # Need to think about activation function
+        x = F.relu(x)
         x = self.fc3(x)
-        return F.softmax(x, dim=1)  # normalized exponential function
+        return F.softmax(x, dim=1)
 
 
 class Agent:
@@ -42,27 +44,28 @@ class Agent:
         self.epsilon_decay = 0
         self.epsilon_min = 0
         self.batch_size = 0
+        self.l_rate = 0.005
+        self.optimizer = None
         self.initial_state = None
-        # network specific params can be moved to the network class,
-        # but it might be handy to have all params in one place.
-        # Make sure to think this through.
+        # initialize parameters and the network
         self.reset()
 
     def reset(self):
         """Initialize the networks and other parameters"""
-        self.initial_state = self.env.reset()   # get initial state from env
-        input_neurons = len(self.initial_state)  # number of input neurons
-        hidden_neurons = 10  # need to think about it
-        output_neurons = len(self.actions)  # number of output neurons
-        self.network = Net(input_neurons=input_neurons,
-                           hidden_neurons=hidden_neurons,
-                           output_neurons=output_neurons)
-
+        self.initial_state = self.env.reset()
+        input_size = len(self.initial_state)
+        hidden1_size = 15
+        hidden2_size = 10
+        output_size = len(self.actions)
+        self.network = Net(input_size, hidden1_size, hidden2_size, output_size)
         self.gamma = 0.95
         self.epsilon = 0.05  #
         self.epsilon_decay = 0.995
         self.epsilon_min = 0.01
         self.batch_size = 16
+        self.l_rate = 0.005
+        self.optimizer = optim.SGD(self.network.parameters(),
+                                   lr=self.l_rate, momentum=0.9)
         pass
 
     def run(self):
@@ -88,7 +91,41 @@ class Agent:
         Note: this method does a training *step*, not whole training
         """
         # TODO implement me!
-        pass
+        if len(self.memory) > self.batch_size:
+            self.optimizer.zero_grad()
+
+            exp_batch = random.sample(self.memory, self.batch_size)
+            input_state_batch = autograd.Variable(
+                [x[0] for x in exp_batch], requires_grad=True)
+            action_batch = autograd.Variable([x[1] for x in exp_batch])
+            reward_batch = autograd.Variable([x[2] for x in exp_batch])
+            next_state_batch = autograd.Variable([x[3] for x in exp_batch],
+                                                 requires_grad=True)
+            is_terminal_state_batch = autograd.Variable(
+                [x[4] for x in exp_batch])
+
+            # for each state in batch calc Q
+            q_t0_values = self.network(input_state_batch)
+
+            # for each next_state in batch calc max{a}{Q(s,a)}
+            q_t1_max = torch.max(self.network(next_state_batch), 0)
+
+            # if next_state - max{a}{Q(s,a)} should be zero
+            q_t1_max_with_terminal = torch.mul(q_t1_max,
+                                               is_terminal_state_batch)
+
+            # now calc the targets for each batch (only for the action
+            # taken in the batch!)
+            targets = reward_batch + self.gamma * q_t1_max_with_terminal
+            loss = torch.zeros(self.batch_size, len(self.actions))
+
+            for sample, i in enumerate(loss):
+                sample[action_batch[i]] = F.mse_loss(
+                    q_t0_values[action_batch[i]], targets[i])
+
+            # 6. train net with the error
+            loss.backward()
+            self.optimizer.step()
 
     def _get_next_action(self, state):
         """
