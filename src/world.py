50582492e5bac0c692546e7ad6cdbe903d1b2ba9
aboutroots <popiel.mw@gmail.com>
2018-05-21 00:26:05 +0200

    better docstrings in world.py

--- src/world.py
+++ src/world.py
@@ -11,9 +11,17 @@ Information for listeners are the daytime and the weather.
 'sun' value is the sun power before calculating with clouds
 'light' value is the sun power after this calculation
 
+
+Note on algorithms:
+
+Most of the weights and biases were determined experimentally.
+The main goal was to provide natural-looking weather, not self-corelated, with
+nicely varied rain periods, clouds, ocassional storms etc.
+In order to achieve this, in some places a modified version of Gillbert-Elliot
+channel model was used to simulate constant periods of given weather phenomena.
+Please refer to its logic first, in case of being lost in code.
+
 """
-# FIXME a lot of unexplained / undocumented calculations going on. Either
-# FIXME add docs to update methods or add abstraction
 import json
 import os
 import random
@@ -26,14 +34,22 @@ class World:
     """Time and weather computations"""
 
     def __init__(self, time_step_in_minutes=None, duration_days=1):
+        """
+        Args:
+            time_step_in_minutes(int): how long a single step will last.
+            duration_days(int): how many days the episode will run. For endless
+                                loop, make it None
 
+        """
+
+        # read config
         add_path = ''
         if 'tests' in os.getcwd():
             add_path = '../'
         with open(add_path + '../configuration.json') as config_file:
             self.config = json.load(config_file)
 
-        self.start_date = datetime(2020, 1, 1, 0, 0, 0)
+        self.start_date = datetime(2020, 1, 1, 0, 0, 0)  # arbitrary start date
         self.stop_date = None
         if duration_days:
             self.stop_date = self.start_date + timedelta(days=duration_days)
@@ -53,6 +69,7 @@ class World:
         self.base_step = timedelta(minutes=self.base_step_in_minutes)
         self.basetime = None
 
+        # compute initial time
         self._compute_daytime()
         self._compute_basetime()
 
@@ -70,6 +87,7 @@ class World:
         self.base_temperature = random.randrange(-10, 30)
         self.tendency = 1  # 1 or -1, the overall temperature will be slowly
         # increasing or decreasing
+        self.sun_amplitude = random.uniform(0.5, 1)
 
         # other settings
         self.listeners = []
@@ -88,12 +106,12 @@ class World:
         """Update all listeners with interpolated weather and current time"""
 
         for listener in self.listeners:
-            # FIXME czy ten try-except jest tutaj potrzebny?
             try:
                 listener.update(daytime=self.daytime,
                                 weather=self.int_weather)
             except AttributeError:
-                raise AttributeError('listener has unimplemented method update')
+                raise AttributeError('listener has unimplemented\
+                                     method update')
 
     def _interpolate_weather(self):
         linear_factor = (
@@ -131,17 +149,20 @@ class World:
         return False
 
     def _compute_daytime(self):
+        # in place
         now = self.current_date
         midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
         self.daytime = (now - midnight).seconds / 60
 
     def _compute_basetime(self):
+        # in place
         base_now = self.base_date
         base_midnight = base_now.replace(hour=0, minute=0, second=0,
                                          microsecond=0)
         self.basetime = (base_now - base_midnight).seconds // 60
 
     def _update_weather(self):
+        """Updates all weather factors in proper order"""
         for key, value in self.weather.items():
             self.old_weather[key] = value
 
@@ -154,21 +175,35 @@ class World:
         self._calculate_temperature()
 
     def _calculate_sun(self):
-        daystart = 300  # NOTE: consider moving globals to config file
-        dayend = 1140
+        """Sun is calculated as a sinus between day start and end hour.
+
+        Amplitude is set once per episode, and varies between <0.5, 1>
+
+        """
+
+        daystart = self.config['env']['day_start']
+        dayend = self.config['env']['day_end']
         daylen = dayend - daystart
         sun = 0
         if daystart <= self.basetime <= dayend:
-            # FIXME sinus amplitude random, calculated once for 1 episode
-            sun = truncate(sin((self.basetime - daystart) * pi / daylen))
+            sun = truncate(sin((self.basetime - daystart) * pi / daylen)) *\
+                  self.sun_amplitude
 
         self.weather['sun'] = sun
 
     def _calculate_wind(self):
+        """Wind is calculated differently when active and inactive.
+
+        We have different propabilities to change between states, and we also
+        include previous value as a factor.
+
+        """
+
         # probabilities to start / stop blowing
         probability_stop = 0.05
         probability_start = 0.02
 
+        # extracted booleans:
         is_blowing = self.weather['wind'] > 0
         going_to_stop = random.uniform(0, 1) < probability_stop
         going_to_start = random.uniform(0, 1) < probability_start
@@ -187,12 +222,24 @@ class World:
             self.weather['wind'] = random.betavariate(2, 5)
 
     def _calculate_clouds(self):
+        """Clouds are calculated differently when active and inactive.
+
+        We have different propabilities to change between states, and we also
+        include previous value and wind as a factor.
+        Altough, there is still a small chance of sudden storm or cleariness.
+
+        Clouds have some critical value, and they tend to not pass through it.
+        This allows us to simulate mainly moderate weather.
+
+        """
+
         probability_stop = 0.1
         probability_start = 0.02
         probability_clear_all = 0.004
         probability_storm = 0.002
         probability_pass_critical = 0.9
 
+        # extracted booleans:
         is_cloudy = self.weather['clouds'] > 0
         going_to_stop = random.uniform(0, 1) < probability_stop
         going_to_start = random.uniform(0, 1) < probability_start
@@ -203,10 +250,8 @@ class World:
         if is_cloudy:
             if suddenly_clear_all:
                 self.weather['clouds'] = 0
-
             elif suddenly_storm:
                 self.weather['clouds'] = random.uniform(0.8, 1)
-
             elif going_to_stop:
                 self.weather['clouds'] = truncate(self.weather['clouds']
                                                   - random.uniform(0.05, 0.2))
@@ -216,19 +261,15 @@ class World:
                 new_factor = 1 - old_factor
 
                 new_cloud = random.betavariate(5, 1)
-
                 # add new cloud
                 clouds = (self.weather['clouds'] * old_factor
                           + new_cloud * new_factor)
-
                 # consider influence of wind
                 clouds -= self.weather['wind'] * wind_factor
-
                 # go go over 0.4 with the probability of passing critical value
                 upper_limit = 0.4
                 if passing_critical:
                     upper_limit = 1
-
                 # update clouds
                 self.weather['clouds'] = truncate(clouds, 0, upper_limit)
 
@@ -237,6 +278,7 @@ class World:
             self.weather['clouds'] = random.uniform(0.05, 0.2)
 
     def _calculate_light(self):
+        """Light is calculated from sun and clouds"""
         clouds_factor = 0.7
 
         self.weather['light'] = truncate(
@@ -244,8 +286,16 @@ class World:
         )
 
     def _calculate_rain(self):
+        """Rain is calculated differently when active and inactive.
+
+        We have constant propability to change between states, and we also
+        include previous value and clouds as a factor. Note, that there must
+        be at least some clouds for rain to fall.
+
+        """
         probability_change_state = 0.05
 
+        # extracted booleans:
         change_state = random.uniform(0, 1) < probability_change_state
         is_raining = self.weather['rain'] > 0
 
@@ -270,6 +320,20 @@ class World:
             self.weather['rain'] = 0
 
     def _calculate_temperature(self):
+        """Calculated based on all previous factors, base and tendency.
+
+        Base is the additional bias, very slowly changing, simulating
+        long-term weather conditions, such as atmospheric fronts.
+
+        Tendency is a direction for the base - it can be increasing (+1)
+        or decreasing (-1). Tendency has a slight chance to change at any
+        point in time.
+
+        Besides, temperature is computed including its previous value and some
+        hard-coded weights for different factors.
+
+        """
+
         # make changes in base temperature according to tendency
         change_tendency_probability = 0.001
         base_change_probability = 0.05
@@ -279,7 +343,6 @@ class World:
 
         if tendency_changing:
             self.tendency = -self.tendency
-
         if base_changing:
             old_factor = 0.9
             new_factor = 1 - old_factor
@@ -291,7 +354,7 @@ class World:
                 -20, 40
             )
 
-        # Calculate all temperature factors
+        # Calculate all temperature factors with their weights.
         # there will be max 8 degrees warmer in a day
         day_heat = 8 * (sin(self.basetime / 230 + 300) / 2 + 0.5)
         sun_heat = 2 * self.weather['light']
@@ -313,7 +376,7 @@ class World:
         )
 
 
-def plot_weather(): # pragma: no cover
+def plot_weather():  # pragma: no cover
     """Plot normalized weather graph in a single episode"""
     temp, sun, light, clouds, rain, wind = [], [], [], [], [], []
 
